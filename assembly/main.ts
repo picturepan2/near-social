a1e0119997e68932fe14d151cc36f58ddc367428
Yan Zhu <picturepan2@hotmail.com>
2020-01-09 03:47:22 +0800

    Update Smart Contract

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,18 +1,73 @@
-import { logging } from "near-runtime-ts";
+import { context, storage, near, PersistentMap, base64 } from "near-runtime-ts";
 // available class: near, context, storage, logging, base58, base64, 
 // PersistentMap, PersistentVector, PersistentDeque, PersistentTopN, ContractPromise, math
-import { TextMessage } from "./model";
+import { Post, PostArray, User } from "./model";
 
-const NAME = ". Welcome to NEAR Protocol chain"
+const NAME: string = "NEAR Social";
+const POST_LIMIT = 20;
+const ID_DIGITS: u32 = 16;
 
-export function welcome(name: string): TextMessage {
-  logging.log("simple welcome test");
-  let message = new TextMessage()
-  const s = printString(NAME);
-  message.text = "Welcome, " + name + s;
-  return message;
+let posts = new PersistentMap<string, Post>("p");
+let postsByUser = new PersistentMap<string, PostArray>("pu");
+let users = new PersistentMap<string, User>("u");
+
+// Get Post
+export function getPost(id: string): Post {
+  return posts.get(id);
 }
 
-function printString(s: string): string {
-  return s;
+// Set Post
+export function setPost(post: Post): void {
+  posts.set(post.id, post);
+}
+
+// Get Post by User
+export function getPostByUser(user: string): PostArray {
+  return postsByUser.get(user);
+}
+
+// Set Post by User
+export function setPostByUser(post: Post): void {
+  let _posts = getPostByUser(post.user).posts;
+
+  if (_posts == null) {
+    _posts = new Array();
+    _posts.push(post);
+  } else {
+    _posts.push(post);
+  }
+
+  let pa = new PostArray();
+
+  pa.posts = _posts;
+  postsByUser.set(post.user, pa);
+}
+
+// Get User of a Post
+export function userOf(id: string): string {
+  let post = getPost(id);
+  let user = post.user;
+  return user;
+}
+
+// Get a Random Id
+function generateRandomId(): string {
+  let buf = near.randomBuffer(ID_DIGITS);
+  let b64 = base64.encode(buf);
+  return b64;
+}
+
+// Add Post
+function createPost(title: string, content: string, published_at: Int32Array, type: string): Post {
+  let post = new Post();
+  let randId = generateRandomId();
+  post.id = randId;
+  post.title = title;
+  post.content = content;
+  post.published_at = published_at; // TODO: Use the timestamp on blockchain
+  post.user = context.sender;
+  post.type = type;
+  setPost(post);
+  setPostByUser(post);
+  return post;
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -1,5 +1,24 @@
 //@nearfile
+// Model: Post
+export class Post {
+  id: string;
+  title: string;
+  content: string;
+  published_at: i32; // TODO: Use the timestamp on blockchain
+  user: string;
+  type: string;
+}
 
-export class TextMessage {
-    text: string;
+// Model: Post Array
+export class PostArray {
+  posts: Array<Post>;
+}
+
+// Model: User
+export class User {
+  id: string;
+  name: string;
+  avatar: string;
+  bio: string;
+  website: string;
 }
